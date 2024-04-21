8bdb3d3441254b81db09cc7ca35bea3f4ae98084
Pedro Souza <pedro.souza4237@gmail.com>
2024-04-21 13:23:52 -0300

    Using http codes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,27 @@
 import http from 'node:http';
 
+const users = [];
+
 const server = http.createServer((req, res) => {
-    return res.end('Hello World!\n');
+    const { url, method } = req;
+
+    if (url === '/users' && method === 'GET') {
+        return res
+        .setHeader('Content-Type', 'application/json')
+        .end(users.length ? JSON.stringify(users) : 'Nenhum usuário cadastrado');
+    }
+
+    if (url === '/users' && method === 'POST') {
+        users.push({
+            id: 1,
+            name: 'John Doe',
+            email: 'johndoe@exemple.com'
+        });
+
+        return res.writeHead(201).end();
+    }
+
+    return res.writeHead(404).end();
 });
 
 server.listen(3333);
