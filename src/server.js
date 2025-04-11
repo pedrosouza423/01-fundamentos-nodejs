1efdcf49682ca060b74a7556d4cb7f8d2ae24930
Pedro Souza <pedro.souza4237@gmail.com>
2025-04-10 23:56:11 -0300

    Application structure

--- src/server.js
+++ src/server.js
@@ -1,7 +1,26 @@
 import http from 'node:http';
 
+const users = [];
+
 const server = http.createServer((req, res) => {
-    return res.end('Hello Ignite');
+    const { method, url } = req;	
+
+    if (method === 'GET' && url === '/users') {
+        return res
+        .setHeader('Content-Type', 'application/json')
+        .end(JSON.stringify(users));
+    }
+
+    if (method === 'POST' && url === '/users') {
+        users.push({
+            id: 1,
+            name: 'John Doe' ,
+            email: 'johndoe@email.com'
+        });
+        return res.writeHead(201).end('Create user');
+    }
+
+    return res.writeHead(404).end();
 });
 
 server.listen(3333);
